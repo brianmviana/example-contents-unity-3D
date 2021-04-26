bc347266bf62ff72d9d8097d43393c48a91199ba
Brian Viana <brianmviana@gmail.com>
2021-04-26 17:27:22 -0300

    Animation module

---
+++ Animation/Assets/Script/Man.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Man : Megaman {
+
+    public bool sample = true;
+
+    public Animator animator;
+
+    void Start() {
+        
+    }
+    void Update() {
+        if (sample) {
+            animator.SetBool("foward", Input.GetKey(KeyCode.D));
+            animator.SetBool("backward", Input.GetKey(KeyCode.A));
+            animator.SetBool("faster", Input.GetKey(KeyCode.LeftShift));
+            if (Input.GetKeyDown(KeyCode.E)) {
+                animator.SetTrigger("wave");
+            }
+        }
+       /* else {
+            float multi = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+            animator.SetFloat("Speed", Input.GetAxis("Horizontal") * multi);
+        }*/
+    }
+}
---
+++ Animation/Assets/Script/Megaman.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Megaman : MonoBehaviour {
+
+    public AudioSource megamanAudio;
+    public AudioClip[] audioClips;
+
+    public void PlayStep(){
+        int index = Random.Range(0, audioClips.Length);
+        megamanAudio.clip = audioClips[index];
+        megamanAudio.pitch = Random.Range(0.8f, 1.1f);
+        megamanAudio.Play();
+    }
+
+}
+
