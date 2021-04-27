2f9e73e29c9cf49550ce0fc1971da219a81f2dad
Brian Viana <brianmviana@gmail.com>
2021-04-27 11:11:44 -0300

    Timeline module

---
+++ Timeline/Timeline/Assets/Scripts/ZombieRagdoll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieRagdoll : MonoBehaviour {
+
+    private Animation anim;
+    private Collider[] colliders;
+    private Rigidbody[] rigidbodies;
+
+    // Start is called before the first frame update
+    void Start() {
+        anim = GetComponent<Animation>();
+        colliders = GetComponentsInChildren<Collider>();
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        DisableRagdoll();
+    }
+
+    private void DisableRagdoll() {
+        anim.enabled = true;
+        for (var i = 0; i < colliders.Length; i++) {
+            colliders[i].isTrigger = true;
+            rigidbodies[i].isKinematic = true;
+        }
+    }
+
+    private void EnableRagdoll() {
+        anim.enabled = false;
+        for (var i = 0; i < colliders.Length; i++) {
+            colliders[i].isTrigger = false;
+            rigidbodies[i].isKinematic = false;
+            rigidbodies[i].AddForce(new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50)), ForceMode.Impulse);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            EnableRagdoll();
+        }        
+    }
+}
